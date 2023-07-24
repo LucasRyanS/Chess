b35866e0a7e0e37628c1ad5deb56e33699b9bbfc
Lucas Ryan <lucasryandkz@gmail.com>
2023-07-24 12:10:21 -0300

    Start to implement Boeard end Pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 import static java.lang.System.*;
@@ -7,11 +8,6 @@ import static java.lang.System.*;
 public class Program {
     public static void main(String[] args) {
 
-        Position pos = new Position(3, 5);
-
-        out.println(pos);
-
-
-
+        Board board = new Board(8, 8);
     }
 }
