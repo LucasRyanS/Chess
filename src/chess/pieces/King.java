3a9d4e33f00c21407958162b9b3e7245a2dd2d60
Lucas Ryan <lucasryandkz@gmail.com>
2023-07-26 16:20:14 -0300

    Possible movies of a piece

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -11,6 +11,12 @@ public class King extends ChessPiece {
     }
 
     public String toString(){
-        return "k";
+        return "K";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
