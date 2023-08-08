e0710aefd1f316d2c9c8c81cbe3821b6e8dbe18b
Lucas Ryan <lucasryandkz@gmail.com>
2023-08-08 18:14:46 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -60,9 +60,15 @@ public class UI {
         printCapturedPieces(captured);
         out.println();
         out.println("Turn: " + chessMatch.getTurn());
-        out.println("waiting player: " + chessMatch.getCurrentPlayer());
-        if (chessMatch.getCheck()){
-            out.println("CHECK!");
+        if (!chessMatch.getCheckMate()) {
+            out.println("waiting player: " + chessMatch.getCurrentPlayer());
+            if (chessMatch.getCheck()) {
+                out.println("CHECK!");
+            }
+        }
+        else {
+            out.println("CHECKMATE!");
+            out.println("Winner: " + chessMatch.getCurrentPlayer());
         }
     }
 
